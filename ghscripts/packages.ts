a56f1f707cb05645dd8bf1c0d383c68abb707e89
justin <justin@163.com>
2025-07-03 15:59:29 +0800

    update

--- ghscripts/packages.ts
+++ ghscripts/packages.ts
@@ -5,8 +5,6 @@ let cwd = process.cwd();
 function go() {
     console.log("run packages");
     console.log(cwd);
-    console.log("1111111111")
-    console.log(process.env.NODE_AUTH_TOKEN)
     fs.copyFileSync(`${cwd}/ghscripts/package.json`, `${cwd}/build/aptos_commission/package.json`);
     fs.copyFileSync(`${cwd}/ghscripts/.npmrc`, `${cwd}/build/aptos_commission/.npmrc`);
 }
