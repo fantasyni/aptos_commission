27909525d8dd1b3c6c204497530ad8b672825deb
justin <justin@163.com>
2025-07-03 10:40:03 +0800

    update

--- ghscripts/packages.ts
+++ ghscripts/packages.ts
@@ -1,1 +1,8 @@
-console.log("run packages");
+import fs from "fs";
+
+function go() {
+    console.log("run packages");
+    fs.copyFileSync("./package.json", "../build/aptos_commission/package.json");
+}
+
+go();
